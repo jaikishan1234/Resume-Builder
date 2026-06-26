ac16fbafb4ee3c02893739fc829a3699f7887415
Jai <jaikishannayak@gmail.com>
2026-06-26 15:40:19 +0530

    feat: add resume management and multi-step form for resume creation
    - Implemented ResumePage for managing resumes with create, delete, and fetch functionalities.
    - Added EducationStep, ExperienceStep, PersonalInfoStep, ProjectsStep, and SkillsStep components for multi-step resume creation.
    - Integrated AI functionality to generate experience and project descriptions.
    - Created StepHeader component for displaying progress in the multi-step form.
    - Enhanced user experience with modals and form validation using react-hook-form.

--- src/types/ai.types.ts
+++ src/types/ai.types.ts
@@ -1,23 +1,27 @@
 export interface GenerateSummaryBody {
-    experienceLevel: string;
-    skills: string[];
-    jobTitle: string
+  experienceLevel: string;
+  skills: string[];
+  jobTitle: string;
 }
 
 export interface GenerateSkillsBody {
-    experienceLevel: string;
-    jobTitle: string
+  experienceLevel: string;
+  jobTitle: string;
 }
 
 export interface GenerateProjectDescriptionBody {
-    experienceLevel: string;
-    jobTitle: string;
-    techStack: string[];
+  experienceLevel: string;
+  jobTitle: string;
+  techStack: string[];
 }
 
 export interface GenerateExperienceDescriptionBody {
-    experienceLevel: string;
-    techStack: string[];
-    yearsOfExperience: number;
-    jobRole: string
+  experienceLevel: string;
+  techStack: string[];
+  yearsOfExperience: number;
+  jobRole: string;
+}
+
+export interface ImproveContentBody {
+  content: string;
 }
